a1ac640dbe97f8f1b303c9504e85c97834469631
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-02-27 14:51:17 +0600

    error handling added

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello')
+const searchButton = () => {
+    const inputValue = document.getElementById('input-value')
+    const errorMessage = document.getElementById('error')
+
+
+    const inputVal = inputValue.value
+    if (isNaN(inputVal)) {
+        // alert('Please enter a valid number')
+        errorMessage.innerText = "Please Enter a valid number"
+    }
+
+
+    // const div = document.createElement('div')
+    // const show = `<h1> ${inputValue.value}</h1>`
+
+    // div.appendChild(show)
+}
