3d4972276d73ca5d4d246b6ec9c95f48cb9dbd4e
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-02-27 20:50:32 +0600

    card added

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
+const main = document.getElementById('main');
+
+
 const searchButton = () => {
     const inputValue = document.getElementById('input-value')
     const errorMessage = document.getElementById('error')
 
 
-    const inputVal = inputValue.value
-    if (isNaN(inputVal)) {
+    const input = parseInt(inputValue.value)
+    if (isNaN(input || input == "")) {
         // alert('Please enter a valid number')
-        errorMessage.innerText = "Please Enter a valid number"
+        errorMessage.innerText = "Please enter a valid number"
+        inputValue.value = ""
+    } else if (input <= 0) {
+        error.innerText = "Please give a positive number"
+        inputValue.value = ""
+    } else {
+        fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${input}`)
+            .then(res => res.json())
+            .then(data => cardsDisplay(data.cards))
+        inputValue.value = ""
     }
 
 
-    // const div = document.createElement('div')
-    // const show = `<h1> ${inputValue.value}</h1>`
 
-    // div.appendChild(show)
+
+}
+
+const cardsDisplay = (cards) => {
+
+    console.log(cards)
+    for (const card of cards) {
+        console.log(card)
+        const div = document.createElement('div')
+        div.classList.add("col-lg-4")
+        div.classList.add("mb-5")
+        div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+  <img src="${card.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${card.suit}</h5>
+    <p class="card-text">${card.code} </p>
+    <a href="#" class="btn btn-primary">See Details</a>
+  </div>
+</div>
+        
+        
+        `
+
+        main.appendChild(div)
+    }
 }
